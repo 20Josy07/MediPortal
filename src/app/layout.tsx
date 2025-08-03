f6454fdead68a3fa6cf1eaa55b8a068655ce6340
Josimar Acosta <josyacosta07@gmail.com>
2025-08-03 14:40:35 +0000

    no actualizaste nada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
