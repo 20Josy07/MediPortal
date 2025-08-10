41ca63014b18544ffbdee27ac9288d683ad4f113
Josimar Acosta <josyacosta07@gmail.com>
2025-08-10 01:48:53 +0000

    el redirect url despues de vincular calendar me manta para https://maste

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -4,7 +4,7 @@ import { google } from 'googleapis';
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.NEXTAUTH_URL + '/api/auth/callback/google'
+  'https://zendapsi.com/api/auth/callback/google'
 );
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
