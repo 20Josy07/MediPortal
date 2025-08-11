c88adaa28b24e85f6bbaebdfc2309d2b98f0028c
Josimar Acosta <josyacosta07@gmail.com>
2025-08-11 20:05:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FormItem classNam

--- src/components/dashboard/session-form.tsx
+++ src/components/dashboard/session-form.tsx
@@ -501,20 +501,6 @@ export function SessionForm({
               </div>
               <FormField
                   control={form.control}
-                  name="remindPsychologist"
-                  render={({ field }) => (
-                      <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                          <div className="space-y-0.5">
-                              <FormLabel>Recordar a psicólogo</FormLabel>
-                          </div>
-                          <FormControl>
-                              <Switch checked={field.value} onCheckedChange={field.onChange} />
-                          </FormControl>
-                      </FormItem>
-                  )}
-              />
-              <FormField
-                  control={form.control}
                   name="remindPatient"
                   render={({ field }) => (
                       <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
