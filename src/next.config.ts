ebfd5f6c299ce01008b071ff93529c508ee08f73
Josimar Acosta <josyacosta07@gmail.com>
2025-08-16 21:00:19 +0000

    arregla la transcripción

---
+++ src/next.config.ts
@@ -0,0 +1,50 @@
+
+import type { NextConfig } from 'next';
+import type { WebpackConfigContext } from 'next/dist/server/config-shared';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  webpack: (config, options: WebpackConfigContext) => {
+    if (!options.isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "handlebars": false,
+        "dotprompt": false,
+      };
+    }
+    return config;
+  },
+  serverActions: {
+    bodySizeLimit: '4mb',
+  },
+};
+
+export default nextConfig;
